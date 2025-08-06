5780e293c5a90023aaf3eba18956a0fd9b4b15b6
Jorge Gallardo <jorgito899@gmail.com>
2025-08-05 23:15:37 -0400

    feature: Se agrega un avance - Falta terminar el script y otros detalles

---
+++ back/script.sql
@@ -0,0 +1,7 @@
+-- Base de datos Plataforma Digital Integrada para la Gestión Unificada del Permiso de Circulación a nivel Nacional
+
+-- Log fiscalización
+
+-- Log multas propietarios
+
+-- Permiso de circulación
