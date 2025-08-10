e6546b04e9e36bf4463bac182e7d53311ace1d62
Jorge Gallardo <jorgito899@gmail.com>
2025-08-10 14:24:53 -0400

    feature: Se dejan los routers listos

---
+++ back/api-back/routers/consultar_logs_obtencion_permisos/consultar_logs_obtencion_permisos.py
@@ -0,0 +1,9 @@
+# Importamos librerías necesarias
+from fastapi import APIRouter, HTTPException, Depends, Request
+
+# Instanciamos el router
+router = APIRouter()
+
+#####################################################
+# Definimos los endpoints del router
+#####################################################
