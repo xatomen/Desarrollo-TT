7142cc920c7b9b31ab85fd960be69501747c3bf8
Jorge Gallardo <jorgito899@gmail.com>
2025-08-13 23:25:00 -0400

    feature: Se agrega funcionalidad de consultar permiso de circulación

--- back/api-back/routers/consultar_permiso_circulacion/consultar_permiso_circulacion.py
+++ back/api-back/routers/consultar_permiso_circulacion/consultar_permiso_circulacion.py
@@ -1,5 +1,10 @@
 # Importamos librerías necesarias
 from fastapi import APIRouter, HTTPException, Depends, Request
+from pydantic import BaseModel
+from datetime import date
+
+from patentes_vehiculares_chile import validar_patente
+import requests
 
 # Instanciamos el router
 router = APIRouter()
@@ -7,3 +12,54 @@ router = APIRouter()
 #####################################################
 # Definimos los endpoints del router
 #####################################################
+
+# Endpoint para consultar el permiso de circulación a la API http://host.docker.internal:5007/consultar_permiso/{ppu}
+@router.get("/consultar_permiso_circulacion/{patente}")
+async def consultar_permiso_circulacion(patente: str):
+    # Validamos la patente
+    if not validar_patente(patente):
+        raise HTTPException(status_code=400, detail="Patente inválida")
+
+    # Realizamos la consulta al servicio externo
+    response = requests.get(f"http://host.docker.internal:5007/consultar_permiso/{patente}")
+    if response.status_code == 404:
+        raise HTTPException(status_code=404, detail="Permiso de circulación no encontrado")
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Error al consultar el permiso de circulación")
+
+    # Calcular vigencia del permiso
+    fecha_actual = date.today()
+    fecha_exp = date.fromisoformat(response.json().get("fecha_expiracion"))
+    if fecha_exp >= fecha_actual:
+        vigencia = True
+    else:
+        vigencia = False
+
+    # Retornamos la información obtenida
+    return{
+        "id": response.json().get("id"),
+        "ppu": response.json().get("ppu"),
+        "rut": response.json().get("rut"),
+        "nombre": response.json().get("nombre"),
+        "fecha_emision": response.json().get("fecha_emision"),
+        "fecha_expiracion": response.json().get("fecha_expiracion"),
+        "valor_permiso": response.json().get("valor_permiso"),
+        "motor": response.json().get("motor"),
+        "chasis": response.json().get("chasis"),
+        "tipo_vehiculo": response.json().get("tipo_vehiculo"),
+        "color": response.json().get("color"),
+        "marca": response.json().get("marca"),
+        "modelo": response.json().get("modelo"),
+        "anio": response.json().get("anio"),
+        "carga": response.json().get("carga"),
+        "tipo_sello": response.json().get("tipo_sello"),
+        "combustible": response.json().get("combustible"),
+        "cilindrada": response.json().get("cilindrada"),
+        "transmision": response.json().get("transmision"),
+        "pts": response.json().get("pts"),
+        "ast": response.json().get("ast"),
+        "equipamiento": response.json().get("equipamiento"),
+        "codigo_sii": response.json().get("codigo_sii"),
+        "tasacion": response.json().get("tasacion"),
+        "vigencia": vigencia
+    }
